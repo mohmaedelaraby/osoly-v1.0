7f2fa192372cca7b544e386874fb1ee553f8193a
mohmaedelaraby <elarabym857@gmail.com>
2024-01-28 21:01:46 +0200

    login page

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react';
 import { RootRoutes } from './routes/routes';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <RootRoutes />
-    </BrowserRouter>
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <RootRoutes />
+      </BrowserRouter>
+    </ChakraProvider>
+  </QueryClientProvider>
+
 );
 
--- src/modules/auth/templete/LoginForm.jsx
+++ src/modules/auth/templete/LoginForm.jsx
@@ -1,35 +1,100 @@
-import { Card } from '@chakra-ui/react'
-import { useFormik } from 'formik';
-import React, { useState } from 'react'
-import { useLoginMutation } from '../hooks/useLoginMutation';
-import { loginValidation } from '../validation/loginValidation';
+import {
+  Button,
+  Card,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { useLoginMutation } from "../hooks/useLoginMutation";
+import { loginValidation } from "../validation/loginValidation";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import "../style/login.scss";
+import logo from "../../../assets/originallogo.png";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
+  const [show, setShow] = useState(false);
+  const nav = useNavigate()
 
-    const [show, setShow] = useState(false);
+  const { mutate, isLoading } = useLoginMutation();
 
-    const { mutate, isLoading } = useLoginMutation();
-  
-    const handleClick = () => setShow(!show);
-  
-    const initialValues = {
-      username: "",
-      password: "",
-    };
-  
-    const formik = useFormik({
-      initialValues: initialValues,
-      validationSchema: loginValidation,
-      onSubmit: (values) => {
-        ///mutate({ username: values.username, password: values.password });
-        console.log(values)
-      }
-    });
+  const handleClick = () => setShow(!show);
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: loginValidation,
+    onSubmit: (values) => {
+      ///mutate({ username: values.username, password: values.password });
+      console.log(values);
+      nav('/home')
+    },
+  });
   return (
-    <Card>
-
+    <Card padding="36px">
+      <form onSubmit={formik.handleSubmit} className="login_form">
+        <div className="login__form__icon__container">
+          <img src={logo} alt="logo" className="login__form__icon" />
+        </div>
+        <FormControl>
+          <Input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            className="login_form_input"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            isInvalid={formik.touched.username && !!formik.errors.username}
+          ></Input>
+          <div className="login_form_warn">
+            {formik.touched.username && formik.errors.username ? (
+              <Text color="#EE2E2E" fontSize="sm" className="">
+                {formik.errors.username}
+              </Text>
+            ) : null}
+          </div>
+        </FormControl>
+        <FormControl marginTop='12px'>
+          <InputGroup>
+            <Input
+              name="password"
+              pr="4.5rem"
+              className="login_form_input"
+              type={show ? "text" : "password"}
+              placeholder="Enter password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              isInvalid={formik.touched.password && !!formik.errors.password}
+            />
+            <InputRightElement>
+              <Button size="sm" variant="unstyled" onClick={handleClick}>
+                {!show ? <ViewIcon /> : <ViewOffIcon />}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <div className="login_form_warn">
+            {formik.touched.password && formik.errors.password ? (
+              <Text color="#EE2E2E" fontSize="sm" className="">
+                {formik.errors.password}
+              </Text>
+            ) : null}
+          </div>
+        </FormControl>
+        <Button className="login_form_btn" type="submit" isLoading={isLoading}>
+          Login
+        </Button>
+      </form>
     </Card>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
