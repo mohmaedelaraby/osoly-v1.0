28a6cf3e945107558d5f3d3fbdde629435881439
mohmaedelaraby <elarabym857@gmail.com>
2024-02-09 22:31:58 +0200

    Pie Chart

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,12 +1,58 @@
+import { Text } from '@chakra-ui/react';
 import React from 'react'
-import '@coreui/coreui/dist/css/coreui.min.css'
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
 
-function PieChart() {
+
+const PieChartComponent =({data}) =>{
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
   return (
     <>
-    pie chart
+     <PieChart width={500} height={500}>
+      <Pie
+        data={data}
+        cx={230}
+        cy={230}
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={200}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+    </PieChart>
     </>
   )
 }
 
-export default PieChart
+export default PieChartComponent
--- src/modules/home/templete/Home.jsx
+++ src/modules/home/templete/Home.jsx
@@ -1,28 +1,35 @@
-import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
+import { Card, CardBody, CardHeader, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import {  FaBuilding, FaHouseUser, FaUser } from "react-icons/fa";
-import '../style/Home.scss'
-import PieChart from "../../../components/Charts/PieChart";
+import { FaBuilding, FaHouseUser, FaUser } from "react-icons/fa";
+import "../style/Home.scss";
+import PieChartComponent from "../../../components/Charts/PieChart";
 const Home = () => {
   const arr = [
     {
       tile: "Users",
       label: "User",
-      icon:<FaUser/>,
+      icon: <FaUser />,
       num: 12,
     },
     {
       tile: "Owners",
       label: "Owner",
-      icon:<FaHouseUser/>,
+      icon: <FaHouseUser />,
       num: 102,
     },
     {
       tile: "Properties",
       label: "Property",
-      icon:<FaBuilding/>,
+      icon: <FaBuilding />,
       num: 1122,
     },
+  ];
+
+  const data = [
+    { name: "Solved ", value: 400 },
+    { name: "Proccisng ", value: 300 },
+    { name: "Rejected ", value: 300 },
+    { name: "Review", value: 200 }
   ];
   return (
     <>
@@ -31,24 +38,25 @@ const Home = () => {
           <div className="home_container_cards">
             {arr.map((item, i) => (
               <div key={i} className="home_container_cards_card">
-                <Card width='80%'>
+                <Card width="90%">
                   <CardHeader paddingBottom="8px">
                     <Heading>
                       <div className="home_container_cards_card_header">
                         <div className="home_container_cards_card_header_icon">
-                        {item.icon}
+                          {item.icon}
                         </div>
                         <div className="home_container_cards_card_header_title">
-                        {item.tile}
+                          {item.tile}
                         </div>
                       </div>
-                      
-                      </Heading>
+                    </Heading>
                   </CardHeader>
-                  <CardBody paddingTop='0px'>
-                    <span className="home_container_cards_card_num">{item.num}</span>
+                  <CardBody paddingTop="0px">
+                    <span className="home_container_cards_card_num">
+                      {item.num}
+                    </span>
                     <span className="home_container_cards_card_label">
-                    {item.label}
+                      {item.label}
                     </span>
                   </CardBody>
                 </Card>
@@ -57,12 +65,32 @@ const Home = () => {
           </div>
           <div className="home_container_charts_table">
             <div className="home_container_charts_table__chart">
-            <Card width='80%'>
-            <PieChart/>
-            </Card>
+              <Card width="100%">
+                <CardHeader paddingBottom="8px">
+                  <Heading>
+                    <Text fontSize="24px" fontWeight="bold">
+                      Tickets
+                    </Text>
+                  </Heading>
+                </CardHeader>
+                <CardBody paddingTop="0px">
+                  <PieChartComponent data={data} />
+                </CardBody>
+              </Card>
             </div>
             <div className="home_container_charts_table__table">
-
+            <Card width="100%">
+                <CardHeader paddingBottom="8px">
+                  <Heading>
+                    <Text fontSize="24px" fontWeight="bold">
+                      Ads
+                    </Text>
+                  </Heading>
+                </CardHeader>
+                <CardBody paddingTop="0px">
+                  <PieChartComponent data={data} />
+                </CardBody>
+              </Card>
             </div>
           </div>
         </div>
