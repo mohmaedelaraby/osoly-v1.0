1afa1d030185b2431d26f08fd3233a031976a14d
mohmaedelaraby <elarabym857@gmail.com>
2024-03-23 06:07:16 +0200

    home structure

--- src/modules/home/templete/Home.jsx
+++ src/modules/home/templete/Home.jsx
@@ -5,6 +5,11 @@ import "../style/Home.scss";
 import PieChartComponent from "../../../components/Charts/PieChart";
 import BarChartComponent from "../../../components/Charts/BarChart";
 import CardWithImg from "../../../components/Cards/CardWithImg";
+import CardWithNumber from "../../../components/Cards/CardWithNumber";
+import moneyIcon from "../../../assets/icons-svgs/money.svg";
+import interfaceIcon from "../../../assets/icons-svgs/interface.svg";
+import houseIcon from "../../../assets/icons-svgs/house.svg";
+import homesIcon from "../../../assets/icons-svgs/homes.svg";
 const Home = () => {
   const currentUserJson = localStorage.getItem("currentUser");
   const homeData = JSON.parse(currentUserJson)
@@ -54,32 +59,15 @@ const Home = () => {
       <div className="home">
         <div className="home_container">
           <div className="home_container_cards">
-            {metricData && metircArr?.map((item, i) => (
+            {/* {metricData && metircArr?.map((item, i) => (
               <div key={i} className="home_container_cards_card">
-                <Card width="90%">
-                  <CardHeader paddingBottom="8px">
-                    <Heading>
-                      <div className="home_container_cards_card_header">
-                        <div className="home_container_cards_card_header_icon">
-                          {item.icon}
-                        </div>
-                        <div className="home_container_cards_card_header_title">
-                          {item.tile}
-                        </div>
-                      </div>
-                    </Heading>
-                  </CardHeader>
-                  <CardBody paddingTop="0px">
-                    <span className="home_container_cards_card_num">
-                     {metricData[i]}
-                    </span>
-                    <span className="home_container_cards_card_label">
-                      {item.label}
-                    </span>
-                  </CardBody>
-                </Card>
+                <CardWithNumber></CardWithNumber>
               </div>
-            ))}
+            ))} */}
+            <CardWithNumber icon={houseIcon} number={250} desc={'إجمالي الوحدات المؤجرة'} bg={'#E2FBD7'}></CardWithNumber>
+            <CardWithNumber icon={homesIcon} number={170} desc={'  إجمالي العقارات'} bg={'#EFF9FF'}></CardWithNumber>
+            <CardWithNumber icon={moneyIcon} number={'20k'} desc={'  تحصيل الشهر الحالي'} bg={'#F3EBFF'}></CardWithNumber>
+            <CardWithNumber icon={interfaceIcon} number={120} desc={'إجمالي التذاكر المفتوحة'} bg={'#FFE4CE'}></CardWithNumber>
           </div>
           <div className="home_container_charts_table">
             <div className="home_container_charts_table__chart">
