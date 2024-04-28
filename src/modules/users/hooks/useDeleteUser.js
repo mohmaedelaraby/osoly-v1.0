320c1364ebbc15db043f042183cb1d0a7f6e3687
mohmaedelaraby <elarabym857@gmail.com>
2024-04-28 06:11:58 +0300

    fix delete in users

--- src/modules/users/hooks/useDeleteUser.js
+++ src/modules/users/hooks/useDeleteUser.js
@@ -1,11 +1,11 @@
 import { useMutation } from "react-query";
 import { useToastMessage } from "../../../hooks/useToastMessage";
-import { deleteEnterprisesUser } from "../../enterpraiseUsers/service/enterprisesUserServices";
+import { deleteUser } from "../service/userServices";
 
 export const useDeleteUser = () => {
     const { successToast, errorToast } = useToastMessage()
 
-    const mutation = useMutation(deleteEnterprisesUser, {
+    const mutation = useMutation(deleteUser, {
         onSuccess: (res) => {
             if (res.status === 201 || res.status === 200) {
                 successToast()
