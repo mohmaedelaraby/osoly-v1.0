a69c5426f077754ba41dd48ce75f7e7930603e9f
mohmaedelaraby <elarabym857@gmail.com>
2024-06-30 15:32:00 +0300

    phone  number vaildtion

--- src/modules/users/validation/schema.jsx
+++ src/modules/users/validation/schema.jsx
@@ -5,7 +5,7 @@ export const userEditValidation = Yup.object({
   lastNameEn: Yup.string().required("errors.name"),
   firstNameAr: Yup.string().required("errors.name"),
   lastNameAr: Yup.string().required("errors.name"),
-  phoneNumber: Yup.string().min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
+  phoneNumber: Yup.string().min(9 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
   email: Yup.string().email().required("errors.email"),
 });
 export const userCreateValidation = Yup.object({
@@ -14,7 +14,7 @@ export const userCreateValidation = Yup.object({
   firstNameAr: Yup.string().required("errors.name"),
   lastNameAr: Yup.string().required("errors.name"),
   password: Yup.string().min(8 , "errors.password_eight").required("errors.password"),
-  phoneNumber: Yup.string().min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
+  phoneNumber: Yup.string().min(9 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
   role: Yup.string().required("errors.role"),
   email: Yup.string().email().required("errors.email"),
   identityId: Yup.string().min(12 , "errors.password_tweleve").max(12 , "errors.password_tweleve").required("errors.identityNumber"),
@@ -24,7 +24,7 @@ export const ownerEditValidation = Yup.object({
   lastNameEn: Yup.string().required("errors.name"),
   firstNameAr: Yup.string().required("errors.name"),
   lastNameAr: Yup.string().required("errors.name"),
-  phoneNumber: Yup.string().min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
+  phoneNumber: Yup.string().min(9 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
   email: Yup.string().email().required("errors.email"),
 });
 
@@ -34,7 +34,7 @@ export const ownerCreateValidation = Yup.object({
   firstNameAr: Yup.string().required("errors.name"),
   lastNameAr: Yup.string().required("errors.name"),
   password: Yup.string().min(8 , "errors.password_eight").required("errors.password"),
-  phoneNumber: Yup.string().min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
+  phoneNumber: Yup.string().min(9 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.phone"),
   role: Yup.string().required("errors.role"),
   email: Yup.string().email().required("errors.email"),
   identityId: Yup.string().min(12 , "errors.password_tweleve").max(12 , "errors.password_tweleve").required("errors.identityNumber"),
