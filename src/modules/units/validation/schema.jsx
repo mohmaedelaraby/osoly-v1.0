cf6540ba6f36bbaf4cd05298607a6f264788f999
mohmaedelaraby <elarabym857@gmail.com>
2024-07-01 17:48:41 +0300

    id 12 digits

--- src/modules/units/validation/schema.jsx
+++ src/modules/units/validation/schema.jsx
@@ -4,7 +4,7 @@ export const unitsValidation = Yup.object({
   name: Yup.string().notRequired(""),
   rent: Yup.number().notRequired(""),
   rentCollectionDate: Yup.string().notRequired(""),
-  electricityAccount: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(11 , "errors.password_eleven").notRequired(""),
+  electricityAccount: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(11 , "errors.password_eleven").max(11 , "errors.password_eleven").notRequired(""),
   waterAccount: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(10 , "errors.password_ten").max(10 , "errors.password_ten").notRequired(""),
   address: Yup.string().notRequired(""),
   space: Yup.number().notRequired(""),
