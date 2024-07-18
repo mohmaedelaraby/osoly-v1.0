1ffe090abbc8fbd15b44b9b58f5b2986b20ef77e
mohmaedelaraby <elarabym857@gmail.com>
2024-07-18 14:20:08 +0300

    identityId 10

--- src/modules/users/validation/schema.jsx
+++ src/modules/users/validation/schema.jsx
@@ -17,7 +17,7 @@ export const userCreateValidation = Yup.object({
   phoneNumber: Yup.string().min(9 , "errors.password_ten").max(9 , "errors.password_ten").required("errors.phone"),
   role: Yup.string().required("errors.role"),
   email: Yup.string().email().required("errors.email"),
-  identityId: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(12 , "errors.password_tweleve").max(12 , "errors.password_tweleve").required("errors.identityNumber"),
+  identityId: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.identityNumber"),
 });
 export const ownerEditValidation = Yup.object({
   firstNameEn: Yup.string().required("errors.name"),
@@ -37,7 +37,7 @@ export const ownerCreateValidation = Yup.object({
   phoneNumber: Yup.string().min(9 , "errors.password_ten").max(9 , "errors.password_ten").required("errors.phone"),
   role: Yup.string().required("errors.role"),
   email: Yup.string().email().required("errors.email"),
-  identityId: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(12 , "errors.password_tweleve").max(12 , "errors.password_tweleve").required("errors.identityNumber"),
+  identityId: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.identityNumber"),
   contractNumber: Yup.string().matches(/^[0-9]+$/, "general.only_num").min(10 , "errors.password_ten").max(10 , "errors.password_ten").required("errors.contractNumber"),
 });
 
